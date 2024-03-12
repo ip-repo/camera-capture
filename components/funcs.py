84a5c9eaaff20b41972045bbbabb0c651d5ade52
ip-repo <123945379+ip-repo@users.noreply.github.com>
2024-03-12 06:16:59 +0200

    Create funcs.py

---
+++ components/funcs.py
@@ -0,0 +1,12 @@
+import sys
+import os 
+def resource_path(relative_path):
+    """ Get absolute path to resource """
+   
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
